bcf262ad5a5fe7f7dbe3a25af2b9276601352741
MariusB <bira_marius_gabriel@yahoo.com>
2024-03-29 21:33:05 +0200

    First commit after setting the teamplate.

---
+++ src/main/java/com/example/fleetmanagementsystem/FleetmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.fleetmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetmanagementsystemApplication.class, args);
+	}
+
+}
