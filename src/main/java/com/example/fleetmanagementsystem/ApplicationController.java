0cfabd41c44d0b1b2badbc54278e3afbbc0f87cc
MariusB <bira_marius_gabriel@yahoo.com>
2024-03-29 22:19:13 +0200

    fixed layout problem with footer sidebar and header

--- src/main/java/com/example/fleetmanagementsystem/ApplicationController.java
+++ src/main/java/com/example/fleetmanagementsystem/ApplicationController.java
@@ -1,2 +1,28 @@
-package com.example.fleetmanagementsystem;public class ApplicationController {
+package com.example.fleetmanagementsystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/widgets")
+    public String widgets(){
+        return "widgets";
+    }
+
+    @GetMapping("/_layout")
+    public String _layout(){
+        return "_layout";
+    }
+
+    @GetMapping("/index2")
+    public String index2(){
+        return "index2";
+    }
 }
--- src/main/java/com/example/fleetmanagementsystem/FleetmanagementsystemApplication.java
+++ src/main/java/com/example/fleetmanagementsystem/FleetmanagementsystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FleetmanagementsystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FleetmanagementsystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FleetmanagementsystemApplication.class, args);
+    }
 
 }
