73106da64a370a0a4f05f001886e7655dde3268e
MariusB <bira_marius_gabriel@yahoo.com>
2024-04-07 23:14:56 +0300

    SQL conected and done inserting to country

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Country values(1,'USA', 'Washington DC',  'North America', 'United States', 'North America');
+insert into Country values(2, 'ROU', 'Bucharest', 'Romania', 'Romanian', 'Europe');
+insert into Country values(3, 'UK', 'London', 'United Kingdom', 'English', 'Europe');
