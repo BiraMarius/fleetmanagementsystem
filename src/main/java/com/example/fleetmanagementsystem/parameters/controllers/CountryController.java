6ff326207457eb64744ca1d6ed61b656882a5a30
MariusB <bira_marius_gabriel@yahoo.com>
2024-04-15 23:21:12 +0300

    Delete record data @25

--- src/main/java/com/example/fleetmanagementsystem/parameters/controllers/CountryController.java
+++ src/main/java/com/example/fleetmanagementsystem/parameters/controllers/CountryController.java
@@ -5,9 +5,7 @@ import com.example.fleetmanagementsystem.parameters.services.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,6 +35,12 @@ public class CountryController {
     @PostMapping("/countries")
     public String save(Country country){
         countryService.save(country);
+        return "redirect:/countries";
+    }
+
+    @RequestMapping(value="countries/delete/{id}", method = {RequestMethod.GET, RequestMethod.DELETE})
+    public String delete(@PathVariable Integer id){
+        countryService.delete(id);
         return "redirect:/countries";
     }
 
