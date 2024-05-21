a20558719c27d69d9c963e375166346d61e2e791
MariusB <bira_marius_gabriel@yahoo.com>
2024-05-21 21:52:30 +0300

    Revert "State service & repository"
    This reverts commit 5869dca78cfc4a50eab5da069c5a3324a6181b67.

--- src/main/java/com/example/fleetmanagementsystem/parameters/repositories/StateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.fleetmanagementsystem.parameters.repositories;
-
-import com.example.fleetmanagementsystem.parameters.models.State;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StateRepository extends JpaRepository<State, Integer> {
-}
