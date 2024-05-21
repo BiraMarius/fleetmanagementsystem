0056244eb0ddd12da92155db2cc8fba3d40b1ce5
MariusB <bira_marius_gabriel@yahoo.com>
2024-05-21 21:38:45 +0300

    Add states done

--- src/main/java/com/example/fleetmanagementsystem/parameters/controllers/StateController.java
+++ src/main/java/com/example/fleetmanagementsystem/parameters/controllers/StateController.java
@@ -1,6 +1,7 @@
 package com.example.fleetmanagementsystem.parameters.controllers;
 
 import com.example.fleetmanagementsystem.parameters.models.State;
+import com.example.fleetmanagementsystem.parameters.services.CountryService;
 import com.example.fleetmanagementsystem.parameters.services.StateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,58 +10,48 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-public class StateController {
-    @Autowired
-    private StateService stateService;
+    @Controller
+    public class StateController {
+        @Autowired private StateService stateService;
+        @Autowired private CountryService countryService;
 
-//    @GetMapping("/states")
-//    @ResponseBody
-//    public List<State> getAll(){
-//        return countryService.getAll();
-//    }
+        public  Model addModelAttribute(Model model){
+            model.addAttribute("states", stateService.findAll());
+            model.addAttribute("countries", countryService.getAll());
+            return model;
+        }
 
-    @GetMapping("/states")
-    public String getAll(Model model){
-        List<State> states = stateService.getAll();
-        model.addAttribute("states", states);
-        return "parameters/states";
+        //Get All States
+        @GetMapping("/parameters/states")
+        public String findAll(Model model){
+            addModelAttribute(model);
+            return "/parameters/states";
+        }
+
+        @GetMapping("/parameters/stateAdd")
+        public String addState(Model model){
+            addModelAttribute(model);
+            return "parameters/stateAdd";
+        }
+
+        @GetMapping("/parameters/state/{op}/{id}")
+        public String editState(@PathVariable Integer id, @PathVariable String op, Model model){
+            addModelAttribute(model);
+            model.addAttribute("state", stateService.findById(id));
+            return "/parameters/state" + op;
+        }
+
+        //Add State
+        @PostMapping(value="/parameters/states")
+        public String addNew(State state) {
+            stateService.save(state);
+            return "redirect:/parameters/states";
+        }
+
+        @RequestMapping(value="/parameters/states/delete/{id}", method = {RequestMethod.DELETE, RequestMethod.GET})
+        public String delete(@PathVariable Integer id) {
+            stateService.delete(id);
+            return "redirect:/parameters/states";
+        }
+
     }
-
-    @GetMapping("/stateAdd")
-    public String addState(){
-        return "parameters/stateAdd";
-    }
-
-    @GetMapping("/countryEdit/{id}")
-    public String editCountry(@PathVariable Integer id, Model model){
-        State state = stateService.getById(id);
-        model.addAttribute("state", state);
-        return "parameters/countryEdit";
-    }
-
-    @PostMapping("/states")
-    public String save(State state){
-        stateService.save(state);
-        return "redirect:/countries";
-    }
-
-    @RequestMapping(value="states/delete/{id}", method = {RequestMethod.GET, RequestMethod.DELETE})
-    public String delete(@PathVariable Integer id){
-        stateService.delete(id);
-        return "redirect:/states";
-    }
-
-    @RequestMapping(value="/states/update/{id}", method={RequestMethod.GET, RequestMethod.PUT})
-    public String delete(State state){
-        stateService.save(state);
-        return "redirect:/states";
-    }
-
-    @GetMapping("/stateDetails/{id}")
-    public String detailsCountry(@PathVariable Integer id, Model model){
-        State state = stateService.getById(id);
-        model.addAttribute("state", state);
-        return "parameters/countryDetails";
-    }
-}
